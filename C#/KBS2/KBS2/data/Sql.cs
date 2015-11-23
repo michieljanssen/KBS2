59d57df1ea3b3ed8e15a753888bd61646d73e80d
BuildTools <unconfigured@null.spigotmc.org>
2015-11-23 21:54:26 +0100

    sql data compleet

--- C#/KBS2/KBS2/data/Sql.cs
+++ C#/KBS2/KBS2/data/Sql.cs
@@ -16,40 +16,13 @@ namespace KBS2.data
         private SqlConnection con;
         private SqlCommand cmd;
         private SqlDataReader reader;
-        
+
         public Sql()
         {
+
             con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
             con.Open();
-            cmd = new SqlCommand("Select * from Student;",con);
-            reader = cmd.ExecuteReader();
-
-            List<int> ids = new List<int>();
-
-            while (reader.Read()) {
-                ids.Add((int)reader.GetValue(0));
-            }
-            reader.Close();
-
-            for (int i = 0; i < ids.Count; i++) {
-                int studentID = ids[i];
-                SqlCommand vakken = new SqlCommand(
-                       "Select v.Id " +
-                       "from Vak v inner join Klas k on v.oplid = k.oplid"+
-                       " inner join Student s on k.id = s.klasid "+
-                       "where s.Id = "  +studentID, con);
-
-                SqlDataReader sqlr = vakken.ExecuteReader();
-
-                Console.WriteLine(studentID);
-                while (sqlr.Read())
-                {
-                    Console.WriteLine(sqlr.GetValue(0));
-                }
-                sqlr.Close();
-
-            }
-            //cmd.Execute
+           
         }
 
         public Student getStudent(String naam)
