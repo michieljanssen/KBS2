3dd38960d939b0b34b9cb2f7b64d57825b0fe59c
Timmerman H <Hugo.t.95@hotmail.com>
2015-11-30 07:51:20 +0100

    Testcases( succes)

--- C#/KBS2/KBS2TEST/model/StudentTests.cs
+++ C#/KBS2/KBS2TEST/model/StudentTests.cs
@@ -24,6 +24,10 @@ namespace KBS2.model.Tests
                 List<VakCijfer> cijferlist = new List<VakCijfer>();
                 cijferlist.Add(new VakCijfer("UML", 2, 4.5));
                 cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 5.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
                 Student a = new Student("Alfa Bet", "S1075802", cijferlist);
                 if (a.ID == null || a.Naam == null || a.Cijfers == null)
                 {
@@ -37,9 +41,52 @@ namespace KBS2.model.Tests
         public class gehaaldeECTest
         {
             [TestMethod()]
-            public void TODO()
+            public void returns19_whenallgradesarepositive()
             {
-                Assert.Fail();
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 6.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gehaaldeEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returns0_whenallgradesarenegative()
+            {
+                int expected = 0;
+                int actual = 18;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 3.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 1.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 2.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 5.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gehaaldeEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returns11_for4positivegrades()
+            {
+                int expected = 11;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 5.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gehaaldeEC();
+                Assert.AreEqual(expected, actual);
             }
         }
 
@@ -47,19 +94,126 @@ namespace KBS2.model.Tests
         public class gemisteECTest
         {
             [TestMethod()]
-            public void TODO()
+            public void returns19_forallgradesnegative()
             {
-                Assert.Fail();
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 2.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 3.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 3.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 5.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gemisteEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returns0_forallgradespositive()
+            {
+                int expected = 0;
+                int actual = 19;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 7.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 8.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 9.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gemisteEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void return8_for2gradesnegative()
+            {
+                int expected = 8;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 5.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gemisteEC();
+                Assert.AreEqual(expected, actual);
+
             }
         }
 
         [TestClass()]
         public class totaalECTest
         {
+
             [TestMethod()]
-            public void TODO()
+            public void returns19_forallgradesnegative()
             {
-                Assert.Fail();
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 2.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 3.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 3.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 5.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.gemisteEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returns19_forallgradespositive()
+            {
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 7.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 8.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 9.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.totaalEC();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void return19_for2gradesnegative()
+            {
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 5.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                actual = a.totaalEC();
+                Assert.AreEqual(expected, actual);
+
+            }
+            [TestMethod()]
+            public void return19_forfailedpluspos()
+            {
+                int expected = 19;
+                int actual = 0;
+                List<VakCijfer> cijferlist = new List<VakCijfer>();
+                cijferlist.Add(new VakCijfer("UML", 2, 4.5));
+                cijferlist.Add(new VakCijfer("UML", 2, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 3, 5.5));
+                cijferlist.Add(new VakCijfer("UML", 1, 6.5));
+                cijferlist.Add(new VakCijfer("UML", 5, 7.4));
+                cijferlist.Add(new VakCijfer("UML", 6, 1.2));
+                Student a = new Student("Alfa Bet", "S1075802", cijferlist);
+                expected = a.totaalEC();
+                actual = a.gehaaldeEC() + a.gemisteEC();
+                Assert.AreEqual(expected, actual);
+
             }
         }
     }
---
+++ C#/KBS2/KBS2TEST/model/cijfer/ToetsCijferTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KBS2.model.cijfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBS2.model.cijfer.Tests
+{
+    [TestClass()]
+    public class ToetsCijferTests
+    {
+        [TestClass()]
+        public class isVoldoendeTest
+        {
+            [TestMethod()]
+            public void returnstrue_whengradebiggerthan5p5()
+            {
+                bool expected = true;
+                bool actual = false;
+                ToetsCijfer testcijfer = new ToetsCijfer("S1075802", "Hugo Timmerman", 8.8, "11 mei");
+                actual = testcijfer.isVoldoende();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returnstrue_whengradeis5p5()
+            {
+
+                bool expected = true;
+                bool actual = false;
+                ToetsCijfer testcijfer = new ToetsCijfer("S1075802", "Hugo Timmerman", 5.5, "11 mei");
+                actual = testcijfer.isVoldoende();
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void returnsfalse_whengradeislowerthan5p5()
+            {
+                bool expected = false;
+                bool actual = true;
+                ToetsCijfer testcijfer = new ToetsCijfer("S1075802", "Hugo Timmerman", 3.5, "11 mei");
+                actual = testcijfer.isVoldoende();
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
