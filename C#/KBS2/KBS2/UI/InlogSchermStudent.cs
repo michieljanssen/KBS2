b1ea0971ea68ed68f9707762601e392bf0f6cf15
JasperKoning <mail@jasperkoning.com>
2016-01-07 21:13:28 +0100

    login hashing so far // werkt nog niet

--- C#/KBS2/KBS2/UI/InlogSchermStudent.cs
+++ C#/KBS2/KBS2/UI/InlogSchermStudent.cs
@@ -6,12 +6,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace KBS2.UI
 {
     public partial class InlogSchermStudent : Form
     {
+        private string wwInput;
+        private byte[] result;
+
         public InlogSchermStudent()
         {
             InitializeComponent();
@@ -39,6 +43,16 @@ namespace KBS2.UI
 
         private void btn_inloggen_Click(object sender, EventArgs e)
         {
+            string wwhash = getCrypt(txtbx_wachtwoord.Text);
+            Console.WriteLine(wwhash);
+            MessageBox.Show(wwhash);
+
+            //wwInput = "";
+            //SHA512 alg = SHA512.Create();
+            //result = alg.ComputeHash(Encoding.UTF8.GetBytes(this.txtbx_wachtwoord.Text));
+            //wwInput = Encoding.UTF8.GetString(result);
+            //Console.WriteLine(wwInput);
+
             //Verberg de huidige form
             this.Hide();
 
@@ -48,6 +62,21 @@ namespace KBS2.UI
 
             //Sluit de applicatie als StudentKijkt wordt afgesloten
             this.Close();
+        }
+
+        public string getCrypt(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] message = UE.GetBytes(text);
+            SHA512Managed hashString = new SHA512Managed();
+            string hexNumber = "";
+            byte[] hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hexNumber += String.Format("{0:x2}", x);
+            }
+            string hash = hexNumber;
+            return hash;
         }
     }
 }
