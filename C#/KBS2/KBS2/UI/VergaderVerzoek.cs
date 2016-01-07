bbe20c30a854caa33cc6706e2bf111d6181168b0
JasperKoning <mail@jasperkoning.com>
2016-01-07 12:28:16 +0100

    Mail functionaliteit

--- C#/KBS2/KBS2/UI/VergaderVerzoek.cs
+++ C#/KBS2/KBS2/UI/VergaderVerzoek.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,7 +26,28 @@ namespace KBS2.UI
 
         private void btn_verstuurBericht_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.office365.com");
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(
+                  "s1082925@student.windesheim.nl", "");
+                MailMessage msg = new MailMessage();
+                msg.To.Add(this.txtbx_emailOntvanger.Text);
+                msg.From = new MailAddress("s1082925@student.windesheim.nl");
+                msg.Subject = this.txtbx_onderwerp.Text;
+                msg.Body = this.txtbx_bericht.Text;
+                client.Send(msg);
+                MessageBox.Show("Successfully Sent Message.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
