604e5ba01f62be2873134340ec1a190feb1a18cf
ThePizzaguy <oscar.van.zanten@windesheim.nl>
2016-01-12 11:28:20 +0100

    Comment pt3

--- C#/KBS2/KBS2/data/StudentSql.cs
+++ C#/KBS2/KBS2/data/StudentSql.cs
@@ -39,41 +39,16 @@ namespace KBS2.data
         //student ophalen uit database
         public static Student getStudent(int id)
         {
+            //verkrijgt  student naam uit sql
             String query = "select *  from student where id = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
-            List<VakCijfer> cijfers = new List<VakCijfer>();
             String naam = (String)reader.GetValue(1);
             reader.Close();
-            ////////////////
-            //query = "select distinct vak.id, vak.ec from student " +
-            //    "INNER JOIN Jaar on Student.Id = Jaar.studentId " +
-            //    "INNER JOIN Klas on Klas.Id = Jaar.klasId " +
-            //    "INNER JOIN Opleiding on Klas.oplid = Opleiding.id " +
-            //    "INNER JOIN Vak on Vak.oplid = Opleiding.ID " +
-            //    "WHERE Student.Id = " + id + ";";
-            //com = new SqlCommand(query, con);
-            //reader = com.ExecuteReader();
-
-            //while (reader.Read())
-            //{
-            //    VakCijfer vakcijfer = new VakCijfer((string)reader.GetValue(0), (int)reader.GetValue(1), new List<ToetsCijfer>());
-            //    cijfers.Add(vakcijfer);
-            //}
-            //reader.Close();
-
-            //query = "select distinct Toets.id, vak.id from Toets " +
-            //    "INNER join Vak on Vak.id = Toets.vakid " +
-            //    "INNER JOIN opleiding on Opleiding.id = vak.oplid " + 
-            //    "INNER JOIN Klas on klas.oplid = Opleiding.id " +
-            //    "INNER JOIN Jaar on jaar.klasid = Klas.ID " +
-            //    "INNER JOIN";
-
-
-
-
-            ///////////
+           
+            //verkrijgt toets informatie uit sql
+            List<VakCijfer> cijfers = new List<VakCijfer>();
             query = "SELECT  Cijfer.toetsid, Cijfer.cijfer,Cijfer.datum, Toets.vakid, Vak.ec " +
             "FROM Student " +
             "INNER JOIN Cijfer ON Student.Id = Cijfer.studentid " +
@@ -82,6 +57,8 @@ namespace KBS2.data
             "WHERE Student.Id = " + id + ";";
             com = new SqlCommand(query, con);
             reader = com.ExecuteReader();
+
+            //voegt de data toe aan de lijst
             while (reader.Read())
             {
                 bool found = false;
@@ -104,71 +81,89 @@ namespace KBS2.data
                 }
             }
             reader.Close();
-            Console.WriteLine(cijfers.Count);
+            //maakt student aan en geeft deze terug
             Student student = new Student(naam, id + "", cijfers);
             return student;
         }
 
         public static Boolean studentExists(String naam)
         {
+            //vraagt informatie op over de student in kwestie
             String query = "select * from Student where naam = '" + naam + "'";
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
+            //checked of er informatie is
             Boolean check = reader.HasRows;
             reader.Close();
+            //geeft antwoord terug
             return check;
         }
         public static Boolean studentExists(int id)
         {
+            //vraagt informatie op over de student in kwestie
             String query = "select * from Student where id = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
+            //checked of er informatie is
             Boolean check = reader.HasRows;
             reader.Close();
+            //geeft antwoord terug
             return check;
         }
 
         public static Boolean passwordCompare(int id, string hash)
         {
+            //vraagt hash op uit sql
             String query = "select hash from Email where studentid = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
             string storedHash = reader.GetString(0);
             reader.Close();
-            bool check = storedHash.Equals(hash);           
+            //vergelijkt de hashes
+            bool check = storedHash.Equals(hash);      
+            //geeft antwoord terug     
             return check;
         }
 
         public static String getEmail(int id)
         {
+            //vraagt informatie op
             String query = "select email from Email where studentid = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
+            //verkrijgt informatie
             string email = reader.GetString(0);
             reader.Close();
+            //geeft informatie terug
             return email;
         }
 
         public static String getStudentNaam(int id)
         {
+            //vraagt informatie op
             String query = "select naam from Student where id = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
+            //verkrijgt informatie
             string naam = reader.GetString(0);
             reader.Close();
+            //geeft naam terug
             return naam;
         }
         public static String getSalt(int id)
         {
+            //vraagt salt op
             String query = "select salt from Email where studentid = " + id;
             SqlCommand com = new SqlCommand(query, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
+            //verkrijgt salt
             string salt = reader.GetString(0);
             reader.Close();
+            //geeft salt terug
             return salt;
         }
     }
